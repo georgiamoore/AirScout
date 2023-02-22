ad599e5f6853cc77dcc43a4833eee8e9fdcfb095
Georgia Moore <mooregeorgia42@gmail.com>
2023-02-22 17:36:52 +0000

    MUI components testing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from 'next/head'
+import dynamic from "next/dynamic";
+const Header = dynamic(() => import("../components/Header"), {
+  loading: () => <p>Loading...</p>,
+  ssr: false,
+});
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
